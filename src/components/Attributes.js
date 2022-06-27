75b3a989b223713948a7ac059b7e91fe4746399c
AlexGP80 <alejandrogamino@protonmail.com>
2022-06-28 00:09:51 +0200

    Added information tooltips for generation type and house ruled weight

--- src/components/Attributes.js
+++ src/components/Attributes.js
@@ -3,53 +3,56 @@ import "../styles/attributes.css";
 import Attribute from "./Attribute";
 
 export default function Attributes(props) {
+  const attrElements = props.attributes.map((att) => (
+    <Attribute key={att.name} name={att.name} value={att.value} />
+  ));
 
-    const attrElements = 
-        props.attributes
-            .map(att => <Attribute
-                            key = {att.name}
-                            name = {att.name}
-                            value = {att.value} 
-                         />);
-
-    function handleHouseRuledWeightChange(event) {
-        if (event.target.value !== props.useHouseRuledWeight) {
-            props.toggleUseHouseRuledWeightChange();
-        }
+  function handleHouseRuledWeightChange(event) {
+    if (event.target.value !== props.useHouseRuledWeight) {
+      props.toggleUseHouseRuledWeightChange();
     }
+  }
 
-    function handleFrameChange(event) {
-        props.setFrame(event.target.value);
-    }
+  function handleFrameChange(event) {
+    props.setFrame(event.target.value);
+  }
 
-    return (
-        <div className="attributes-container">
-            <h2>Attributes</h2>
-            <label htmlFor="useHouseRuledWeight">Use House Ruled Weight</label>
-            <select
-                id="useHouseRuledWeight"
-                name="useHouseRuledWeight"
-                value={props.useHouseRuledWeight}
-                onChange={handleHouseRuledWeightChange}
-            >
-                <option value={false}>false</option>
-                <option value={true}>true</option>
-            </select>
-            {(props.useHouseRuledWeight === false) && 
-            <div>
-                <label htmlFor="frame">Frame</label>
-                <select
-                    id="frame"
-                    name="frame"
-                    value={props.frame}
-                    onChange={handleFrameChange}
-                >
-                    <option value="Lithe">Lithe</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Heavy">Heavy</option>
-                </select>
-            </div>}
-            {attrElements}
+  return (
+    <div className="attributes-container">
+      <h2>Attributes</h2>
+      <span
+        className="info"
+        title="Homebrewed rule with no frames, weight is computed from SIZ, STR and CON values in accordance with Corpulence Index"
+      >
+        &#128712;
+      </span>
+      &nbsp;
+      <label htmlFor="useHouseRuledWeight">Use House Ruled Weight&nbsp;</label>
+      <select
+        id="useHouseRuledWeight"
+        name="useHouseRuledWeight"
+        value={props.useHouseRuledWeight}
+        onChange={handleHouseRuledWeightChange}
+      >
+        <option value={false}>false</option>
+        <option value={true}>true</option>
+      </select>
+      {props.useHouseRuledWeight === false && (
+        <div>
+          <label htmlFor="frame">Frame&nbsp;</label>
+          <select
+            id="frame"
+            name="frame"
+            value={props.frame}
+            onChange={handleFrameChange}
+          >
+            <option value="Lithe">Lithe</option>
+            <option value="Medium">Medium</option>
+            <option value="Heavy">Heavy</option>
+          </select>
         </div>
-    );
+      )}
+      {attrElements}
+    </div>
+  );
 }
--- src/components/Characteristics.js
+++ src/components/Characteristics.js
@@ -3,61 +3,70 @@ import Characteristic from "./Characteristic";
 import "../styles/characteristics.css";
 
 export default function Characteristics(props) {
+  const characteristicsElements = props.characteristics.map(
+    (characteristic) => (
+      <Characteristic
+        key={characteristic.name}
+        name={characteristic.name}
+        value={characteristic.value}
+        min={characteristic.min}
+        max={characteristic.max}
+        incDecCharacteristic={props.incDecCharacteristic}
+        generationType={props.generationType}
+      />
+    )
+  );
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
-
-    const characteristicsElements = 
-            props.characteristics.map(
-                characteristic => <Characteristic 
-                                        key={characteristic.name}
-                                        name={characteristic.name} 
-                                        value={characteristic.value}
-                                        min={characteristic.min}
-                                        max={characteristic.max}
-                                        incDecCharacteristic={props.incDecCharacteristic}
-                                        generationType={props.generationType}
-                                    />
-            );
-
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
-        
-        
-    return (
-        <div className="characteristics-container">
-            <h2>Characteristics</h2>
-            <form className="form" onSubmit={handleSubmit}>
-                <label htmlFor="generationType">Generation Type</label>
-                <select 
-                    id="generationType" 
-                    name="generationType"
-                    value={props.generationType}
-                    onChange={props.handleGenTypeChange}
-                >
-                    <option value="points75">Points: 75</option>
-                    <option value="points80">Points: 80</option>
-                    <option value="stdRoll">Roll: Standard</option>
-                    <option value="manual">Manual</option>
-                </select>
-                {props.generationType.startsWith("points") &&
-                <div className="remaining">
-                    <label 
-                        htmlFor="remainingPoints"
-                        className="remainingLabel"
-                    >Remaining Points</label>
-                    <input 
-                        type="text"
-                        name="remainingPoints"
-                        value={props.remainingPoints}
-                        className="remainingValue"
-                        readOnly
-                    />
-                </div>}
-                {props.generationType === "stdRoll" &&
-                <button onClick={props.rollCharacteristics}>Roll</button>}
-                {characteristicsElements}
-            </form>
-        </div>
-    );
+  return (
+    <div className="characteristics-container">
+      <h2>Characteristics</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <span
+          className="info"
+          title="Points: 75 -> distribute 75 points amongst the characteristics.
+Points: 80 -> distribute 80 points amongst the characteristics.
+Roll: Standard -> Roll 3d6 for STR, CON, DEX, POW & CHA, roll 2d6+6 for SIZ & INT.
+Manual -> set the characteristics values manually."
+        >
+          &#128712;
+        </span>
+        &nbsp;
+        <label htmlFor="generationType">Generation Type</label>
+        <select
+          id="generationType"
+          name="generationType"
+          value={props.generationType}
+          onChange={props.handleGenTypeChange}
+        >
+          <option value="points75">Points: 75</option>
+          <option value="points80">Points: 80</option>
+          <option value="stdRoll">Roll: Standard</option>
+          <option value="manual">Manual</option>
+        </select>
+        &nbsp;
+        {props.generationType.startsWith("points") && (
+          <div className="remaining">
+            <label htmlFor="remainingPoints" className="remainingLabel">
+              Remaining Points
+            </label>
+            <input
+              type="text"
+              name="remainingPoints"
+              value={props.remainingPoints}
+              className="remainingValue"
+              readOnly
+            />
+          </div>
+        )}
+        {props.generationType === "stdRoll" && (
+          <button onClick={props.rollCharacteristics}>Roll</button>
+        )}
+        {characteristicsElements}
+      </form>
+    </div>
+  );
 }
